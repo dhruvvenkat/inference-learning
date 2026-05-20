13389546a6b387ee5500c9bc043e2604512f0882
dvenkat <dhruv.venkat.2024@gmail.com>
2026-05-20 13:39:29 -0600

    setup matmul project

---
+++ matmul/include/matrix.hpp
@@ -0,0 +1,14 @@
+#define MATRIX_HEADER
+#ifndef MATRIX_HEADER
+
+class Matrix {
+    private:
+                
+    
+    public:
+        int[][] multiplyMatrices (int[][] matrix1, int[][] matrix2);
+                
+}
+
+
+#endif
